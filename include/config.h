74107c70fa05dd7f839f2d16c92eb22d4f88954e
forbin1ab <forbin1ab@gmail.com>
2022-10-26 13:35:52 -0500

    start of changes

--- include/config.h
+++ include/config.h
@@ -17,8 +17,8 @@
                                                           // Needs platform = espressif32@3.1.0 !!!!
 
 // Define (just one) type of display.  See documentation.
-#define BLUETFT                                           // Works also for RED TFT 128x160
-//#define ST7789                                          // 240x240 TFT
+//#define BLUETFT                                           // Works also for RED TFT 128x160
+#define ST7789                                          // 240x240 TFT
 //#define OLED1306                                        // 64x128 I2C OLED SSD1306
 //#define OLED1309                                        // 64x128 I2C OLED SSD1309
 //#define OLED1106                                        // 64x128 I2C OLED SH1106
